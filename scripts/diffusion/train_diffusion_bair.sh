6d0b25f334eaf54f8f8c6d96bb9035998a6d5e64
hujunyao <hujunyao@cumt.edu.cn>
2023-07-13 13:12:05 +0800

    update

---
+++ scripts/diffusion/train_diffusion_bair.sh
@@ -0,0 +1,19 @@
+# sh ./scripts/diffusion/train_diffusion_bair.sh
+
+# 从头训练
+python ./scripts/diffusion/run.py \
+    --flowae_checkpoint ./logs_training/flow_pretrained/bair64/snapshots/RegionMM.pth \
+    --config ./config/bair64.yaml \
+    --log_dir ./logs_training/diffusion \
+    --device_ids 0,1 \
+    --postfix onlyflow
+
+# 预训练
+# python ./scripts/diffusion/run.py \
+#     --flowae_checkpoint ./logs_training/flow_pretrained/kth64/snapshots/RegionMM.pth \
+#     --checkpoint ./logs_training/diffusion/kth64_test_without_rf/snapshots/flowdiff.pth \
+#     --config ./config/kth64.yaml \
+#     --log_dir ./logs_training/diffusion \
+#     --device_ids 0,1 \
+#     --postfix test_without_rf \
+#     --set-start True
