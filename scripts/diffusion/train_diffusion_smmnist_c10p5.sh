c3929d7815025fa359f73f7fee651c173c58c266
hujunyao <06192081@cumt.edu.cn>
2023-08-04 14:43:42 +0800

    [Update] for multi gpus

---
+++ scripts/diffusion/train_diffusion_smmnist_c10p5.sh
@@ -0,0 +1,24 @@
+# CUDA_VISIBLE_DEVICES=2,3 sh ./scripts/diffusion/train_diffusion_smmnist_c10p5.sh
+
+# FLOWCKPT=/mnt/sda/hjy/flow_pretrained
+FLOWCKPT=/home/u009079/zzc/data/vidp/flow_pretrained # hpc_403
+
+# 从头训练
+# python ./scripts/diffusion/run.py \
+#     --flowae_checkpoint $FLOWCKPT/smmnist64/snapshots/RegionMM.pth \
+#     --config ./config/smmnist64.yaml \
+#     --log_dir ./logs_training/diffusion \
+#     --device_ids 0,1 \
+#     --random-seed 1234 \
+#     --postfix DM_Batch32_lr2e-4_c10p5
+
+# 预训练
+python ./scripts/diffusion/run.py \
+    --set-start True \
+    --flowae_checkpoint $FLOWCKPT/smmnist64/snapshots/RegionMM.pth \
+    --checkpoint ./logs_training/diffusion/smmnist64_DM_Batch32_lr2e-4_c10p5/snapshots/flowdiff.pth \
+    --config ./logs_training/diffusion/smmnist64_DM_Batch32_lr2e-4_c10p5/smmnist64.yaml \
+    --log_dir ./logs_training/diffusion \
+    --device_ids 0,1 \
+    --random-seed 1234 \
+    --postfix DM_Batch32_lr2e-4_c10p5
