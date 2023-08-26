b852e75fd9c859e1f9500a78357d763b8a88868b
NKUCVLAB_Ubuntu16 <hujunyao@cumt.edu.cn>
2023-08-27 03:04:21 +0800

    [Update] city flow config

--- scripts/flow/train_flow_cityscape.sh
+++ scripts/flow/train_flow_cityscape.sh
@@ -1,19 +1,21 @@
 # sh ./scripts/flow/train_flow_cityscape.sh
 
-# 从头训练
-# python ./scripts/flow/run.py \
-#     --checkpoint /mnt/sda/hjy/pth/ted-youtube384.pth \
-#     --config ./config/cityscapes128.yaml \
-#     --log_dir ./logs_training/flow \
-#     --device_ids 0,1 \
-#     --postfix test 
+CKPTPATH=/mnt/sda/hjy/pth
 
-# 从0训练
+# 从头训练
 python ./scripts/flow/run.py \
+    --checkpoint $CKPTPATH/taichi256.pth \
     --config ./config/cityscapes128.yaml \
     --log_dir ./logs_training/flow \
     --device_ids 0,1 \
-    --postfix perspective
+    --postfix FlowAE_Batch128_lr2e-4_Region10_affine
+
+# 从0训练
+# python ./scripts/flow/run.py \
+#     --config ./config/cityscapes128.yaml \
+#     --log_dir ./logs_training/flow \
+#     --device_ids 0,1 \
+#     --postfix perspective
 
 # 预训练
 # python ./scripts/flow/run.py \
