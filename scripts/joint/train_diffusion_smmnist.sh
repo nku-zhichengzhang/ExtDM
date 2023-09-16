50ed0bd41f9ffe298445470c7ae3cc9828cf2b99
NKUCVLAB_Ubuntu16 <hujunyao@cumt.edu.cn>
2023-09-16 17:29:58 +0800

    joint_sh

--- scripts/joint/train_diffusion_smmnist.sh
+++ scripts/joint/train_diffusion_smmnist.sh
@@ -1,26 +1,29 @@
 # sh ./scripts/diffusion/train_diffusion_smmnist.sh
+SAVEROOT=./logs_training/diffusion
 
-FLOWCKPT=/mnt/rhdd/zzc/data/video_prediction/flow_pretrained/better # u8
-# FLOWCKPT=/mnt/sda/hjy/flow_pretrained # u11
-# FLOWCKPT=/mnt/sda/hjy/flow_pretrained # u16
-# FLOWCKPT=/home/u009079/zzc/data/vidp/flow_pretrained # hpc_403
+FLOWCKPT=/mnt/sda/hjy/flow_pretrained/better
+        #u8  /mnt/rhdd/zzc/data/video_prediction/flow_pretrained/better
+        #u11 /mnt/sda/hjy/flow_pretrained 
+        #u12 /mnt/sda/hjy/flow_pretrained/better
+        #u15 /mnt/sda/hjy/data/flow_pretrained/better
+        #u16 /mnt/sda/hjy/flow_pretrained/better
+        #u22 /home/ubuntu/zzc/data/video_prediction/flow_pretrained/better
 
-# 从头训练
-# python ./scripts/diffusion/run.py \
-#     --flowae_checkpoint $FLOWCKPT/smmnist64/snapshots/RegionMM.pth \
-#     --config ./config/smmnist64.yaml \
-#     --log_dir ./logs_training/diffusion \
-#     --device_ids 0,1 \
-#     --random-seed 1234 \
-#     --postfix DM_Batch32_lr2e-4_c10p4
+DIFFCKPT=/mnt/sda/hjy/diff_pretrained
+        #u8  /mnt/rhdd/zzc/data/video_prediction/diff_pretrained
+        #u11 /mnt/sda/hjy/flow_pretrained 
+        #u12 /mnt/sda/hjy/diff_pretrained
+        #u15 /mnt/sda/hjy/data/diff_pretrained
+        #u16 /mnt/sda/hjy/diff_pretrained
+        #u22 /home/ubuntu/zzc/data/video_prediction/diff_pretrained
 
 # 预训练
 python ./scripts/diffusion/run.py \
     --set-start True \
     --flowae_checkpoint $FLOWCKPT/smmnist64/snapshots/RegionMM.pth \
-    --checkpoint ./logs_training/diffusion/smmnist64_DM_Batch32_lr2e-4_c10p10/snapshots/flowdiff_0032_S135000.pth \
-    --config ./logs_training/diffusion/smmnist64_DM_Batch32_lr2e-4_c10p10/smmnist64.yaml \
+    --checkpoint $DIFFCKPT/smmnist64_DM_Batch32_lr2e-4_c10p10/flowdiff_0032_S202500.pth* \
+    --config ./config/smmnist64.yaml \
     --log_dir ./logs_training/diffusion \
     --device_ids 0,1 \
     --random-seed 1234 \
-    --postfix DM_Batch32_lr2e-4_c10p10
+    --postfix DMjoint_Batch32_lr2e-4_c10p10
